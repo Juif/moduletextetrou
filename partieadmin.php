17bf23f797750542277c9991fd76bc69851bbd3c
camilleadam006 <cam.adam@outlook.fr>
2017-03-08 15:34:20 +0100

    Create partieadmin.php

---
+++ partieadmin.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<script src="jquery-3.1.1.js"></script>
+	<script src="module_texteTrou2.js"></script>
+
+</head>
+
+<body>
+
+<form action="module_texteTrou2.html" method="post">
+	<p>
+	<label for="titre">Titre</label> : <input type="text" name="titre" id="titre"/>
+	<label for="exo">Exercice</label> : <input type="text" name="exo" id="exo"/>
+	<input type="submit" name="Sauvegarder"/>
+	</p>
+</form>
+	
+</body>
+</html>
