55883e8543ec3f34fdfec259487bb01e3392afe6
Juif <tenoudjib@hotmail.fr>
2017-03-08 14:04:15 +0100

    Update module_texteTrou.js

--- module_texteTrou.js
+++ module_texteTrou.js
@@ -1,1 +1,19 @@
 
+$(document).ready(function(){
+var hideTips= ["<li>indice premier</li> ", "<li>indice second</li>"];
+var i=0;
+var nmbreIndices = hideTips.length;
+$buttonVar=$('button');
+$indiceVar=$('.indice')
+
+$buttonVar.click(function(){
+	if(i<hideTips.length){
+		$indiceVar.append(hideTips[i]);
+		nmbreIndices--;
+		$buttonVar.html('Indice restant '+ nmbreIndices);
+		i++;
+
+
+}
+	})
+						});
